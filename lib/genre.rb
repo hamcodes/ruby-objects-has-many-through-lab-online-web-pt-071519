1a90982c6d007f577caed2491431e92562069ff1
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-08-18 08:06:54 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -2,13 +2,13 @@ class Genre
   attr_accessor :name
   @@all = []
     
-    def self.all
-    @@all
-  end
-  
   def initialize(name)
     @name = name
     @@all << self
+  end
+  
+  def self.all
+    @@all
   end
   
   def songs
